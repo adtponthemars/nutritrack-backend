9e0ff09c0623fe67cea8ab085a41796b302516be
adtponmars <adtp971@gmail.com>
2026-07-11 21:07:04 +0530

    Update firebaseAdmin.js

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -2,7 +2,8 @@ import admin from 'firebase-admin';
 import dotenv from "dotenv";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-const serviceAccount = require("../../serviceAccountKey.json");
+// const serviceAccount = require("../../serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 dotenv.config();
 
