44e93ba665c32c92ec6be9bea26633c4e6db8c14
adtponmars <adtp971@gmail.com>
2026-07-14 21:06:06 +0530

    update cors config

--- src/server.js
+++ src/server.js
@@ -6,10 +6,15 @@ import foodRecommendRoute from './routes/foodRecommendRoute.js'
 import mongoose from "mongoose";
 import cors from "cors";
 import "./config/firebaseAdmin.js"
+import { credential } from "firebase-admin";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: `https://nutritrack-frontend-chi.vercel.app/`,
+  credential: true
+}
+));
 
 // ROUTES
 app.use("/api/foodlog", foodLogRoutes);
@@ -22,8 +27,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.error("MongoDB connection failed:", err));
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection failed:", err));
 
 app.listen(5000, () => console.log("Server running on port 5000"));
 
